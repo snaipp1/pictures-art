ee01f0f266ad4abcc6cb8dc9fdc6e31edc816d96
snaipp1 <snaipp1@yandex.by>
2021-05-25 23:41:06 +0300

    feat: add show more styles module

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import showMoreStyles from './modules/showMoreStyles';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -7,4 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.feedback-slider-item', 'horizontal','.main-prev-btn','.main-next-btn');
     sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"');
+    showMoreStyles('.button-styles', '#styles .row');
 });
---
+++ src/js/modules/showMoreStyles.js
@@ -0,0 +1,32 @@
+import {getResource} from '../services/requests';
+
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
+
+
+    btn.addEventListener('click', function() {
+        getResource('http://localhost:3000/styles')
+            .then(res => createCards(res))
+            .catch(error => console.log(error));
+        this.remove();
+    });
+
+    function createCards(response) {
+        response.forEach(({title, src, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+            card.innerHTML = `
+            <div class="styles-block">
+                <img src="${src}" alt="${title}">
+                <h4>${title}</h4>
+                <a href="${link}">Подробнее</a>
+            </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
+        });
+    }
+};
+
+export default showMoreStyles;
